c244c528b64eba6083ea7a4c7b2b3a9029aa6f23
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-03-15 20:43:05 +0900

    領域指定を試す

--- index.ios.js
+++ index.ios.js
@@ -1,28 +1,16 @@
 import React, { Component } from'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import { AppRegistry, View } from 'react-native'
 
 class TODOApp extends Component {
     render() {
         return(
-            <View style={{alignItems: 'center'}}>
-                <Text style={styles.red}>just red</Text>
-                <Text style={styles.bigblue}>just bigblue</Text>
-                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+            <View>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+                <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    bigblue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-    },
-    red: {
-        color: 'red',
-    },
-})
 
 AppRegistry.registerComponent('TODOApp', () => TODOApp)
